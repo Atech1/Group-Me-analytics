17ed748d76a95a39fd94acff82b3b7603a441055
Atech1 <Atech1@users.noreply.github.com>
2018-06-12 22:23:10 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ SETTINGS_PRIORITY = 1
 PYFORMS_STYLESHEET = 'style.css'
 PYFORMS_STYLESHEET_WINDOWS = 'style.css'
 URL = "https://api.groupme.com/v3"
-Token = "?token=io6CBc8zJP91UFf2B76NFAYj0ooHwB2BJv8SU4fX"
+Token = "?token=io6CBc8zJP91UFfAYj0ooHwB2BJv8SU4fX"
